917a8ca9dcef05675fd2dea77d3ca40310fee222
Zameel Zulfi <67381786+Zameel118@users.noreply.github.com>
2025-06-24 21:51:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,16 @@ def get_slot_machine_spin(rows, cols, symbols):
             column.append(value)
 
         columns.append(column)
+
     return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], "|")
+            else:
+                print(column[row])
 
 def deposit():
     while True:
